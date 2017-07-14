12933517abaa7e700b998902d38c7ddf30189f61
mantuonweb <mantu.nigam@globallogic.com>
2017-07-14 16:35:18 +0530

    some minor change

---
+++ parameter/Validation.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+class Validation {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    @validate
+    greet(@required name: string) {
+        return "Hello " + name + ", " + this.greeting;
+    }
+}
+function validate (target, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log(target,propertyKey,descriptor) 
+}
+function required(target: Object, propertyKey: string | symbol, parameterIndex: number){
+    console.log(target,propertyKey,parameterIndex);
+}
+
+
+// const requiredMetadataKey = Symbol("required");
+
+// function required(target: Object, propertyKey: string | symbol, parameterIndex: number) {
+//     let existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+//     existingRequiredParameters.push(parameterIndex);
+//     Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, propertyKey);
+// }
+
+// function validate(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) {
+//     let method = descriptor.value;
+//     descriptor.value = function () {
+//         let requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
+//         if (requiredParameters) {
+//             for (let parameterIndex of requiredParameters) {
+//                 if (parameterIndex >= arguments.length || arguments[parameterIndex] === undefined) {
+//                     throw new Error("Missing required argument.");
+//                 }
+//             }
+//         }
+//     }
+// }
+var val= new Validation("Hi");
+val.greet("mantu");

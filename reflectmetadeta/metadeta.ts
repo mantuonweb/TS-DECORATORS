7ffe5cac8156e9eea8211ddfa59e0bcb62e4ecda
mantuonweb <mantu.nigam@globallogic.com>
2017-07-17 15:36:06 +0530

    some minor changes

---
+++ reflectmetadeta/metadeta.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+
+function serializeWith(serializer: (input: any) => string) : (target: any, propertyKey: string) => void {
+    return function(target: any, propertyKey: string) {
+        console.log("serializeWith called: adding metadata");
+        Reflect.defineMetadata("serialization", serializer, target, propertyKey);
+    }
+}
+
+
+function MyDateSerializer(value : any) : string {
+    console.log("MyDateSerializer called");
+    return "bla";
+}
+
+class Greeter {
+    @serializeWith(MyDateSerializer)
+    public greeting : string;
+
+    constructor(message: string) {
+        console.log("Greeter constructor");
+        this.greeting = message;
+    }
+}
+
+var greetingInstance = new Greeter("hi");
+
+var serializerFunc : (input: any) => string = Reflect.getMetadata("serialization", greetingInstance, "greeting");
+
+var serializedValue = serializerFunc(greetingInstance.greeting);
+console.log(serializedValue);
